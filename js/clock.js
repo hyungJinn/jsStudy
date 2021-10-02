6bf6d923af7753b3c130d9741e9c23344c2112ad
HyungJin <hyungjin0710@gmail.com>
2021-10-03 04:32:43 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,5 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector(".time");
+const date = document.querySelector(".date");
 
 function getClock() {
   const date = new Date();
@@ -10,3 +11,16 @@ function getClock() {
 
 getClock();
 setInterval(getClock, 1000);
+
+function getDate() {
+  const weeks = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+  const today = new Date();
+  const year = String(today.getFullYear());
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+  const dayOfTheWeek = String(weeks[today.getDay()]);
+  date.innerText = `${year}-${month}-${day} ${dayOfTheWeek}`;
+}
+
+getDate();
